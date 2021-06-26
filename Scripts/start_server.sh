5fecb08eee2285e4d1aa33dc435eb66e5e795b55
petscube <petscube@gmail.com>
2021-06-26 06:08:28 -0700

    third

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DOTNET_CLI_HOME=/temp
 cd /home/ubuntu/petscube/petscube/ApoptionBackend
-sudo dotnet run > /dev/null 2> /dev/null < /dev/null & 
+sudo dotnet run --urls http://0.0.0.0:5000 > /dev/null 2> /dev/null < /dev/null & 
