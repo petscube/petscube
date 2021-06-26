2b1a46cdf8e65a1e6e4eebd459dda347df3ff762
petscube <petscube@gmail.com>
2021-06-26 04:29:12 -0700

    third

--- ApoptionBackend/middlewares/ExceptionMiddleware.cs
+++ ApoptionBackend/middlewares/ExceptionMiddleware.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Http;
+using Realms.Sync.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AdoptionBackend.middlewares
